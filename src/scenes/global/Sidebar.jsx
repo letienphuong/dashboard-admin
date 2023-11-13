f00d663c005a1e9f9308f7df6286109bd172b884
phuong tien <letienphuong1604@gmail.com>
2023-11-13 22:54:43 +0700

    data

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,7 @@
+
+
+const Sidebar = () => {
+    return <div>Side bar</div>
+}
+
+export default Sidebar;
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createContext, useState, useMemo } from 'react'
+import { createTheme } from '@mui/material/styles';
+
+import React from 'react'
+
+export default function theme() {
+  return (
+    <div>theme</div>
+  )
+}
